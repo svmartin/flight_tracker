ae04a79400b6ca9b9115113c4a8d192650ab04d0
SM <sean.martin@gmail.com>
2017-03-11 09:52:51 -0600

    updated timepicker js

---
+++ public/js/timepicker_custom.js
@@ -0,0 +1,11 @@
+$('.timepicker').timepicker({
+    timeFormat: 'HH:mm',
+    interval: 60,
+    minTime: '10',
+    maxTime: '6:00pm',
+    defaultTime: '11',
+    startTime: '10:00',
+    dynamic: true,
+    dropdown: true,
+    scrollbar: true
+});
